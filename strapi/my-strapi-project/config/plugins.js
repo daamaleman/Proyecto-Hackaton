f01b78dc3baee313445b6e2626b020b411819259
dmunoz2019 <dmunozdelrio@gmail.com>
2024-08-08 03:15:20 +0000

    initial set up

---
+++ strapi/my-strapi-project/config/plugins.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'documentation': {
+    enabled: true,
+    config: {
+      // Puedes personalizar las opciones aquí
+      // La configuración predeterminada debería funcionar en la mayoría de los casos
+    },
+  },
+};
